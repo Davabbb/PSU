0256f757aa641258ce9a60c389943509f3d0f5a0
David Beylin <57668741+davabey@users.noreply.github.com>
2021-10-19 21:37:39 +0500

    1_4

---
+++ 1_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int N, A, B;
+    int min = 2;
+    std::cin >> N;
+    while (N % min != 0) {
+        min += 1;
+    }
+    B = (N / min) * (min - 1);
+    A = N - B;
+    std::cout << A << " " << B << std::endl;
+    return 0;
+}
