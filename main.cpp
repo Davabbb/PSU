20a90e1a602a29d15f9130efff74d34ca41f2e08
David Beylin <57668741+davabey@users.noreply.github.com>
2021-10-21 11:56:34 +0500

    GG

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m, find;
+    int count = 0;
+
+    std::cin >> n;
+    int* data1 = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> data1[i];
+    }
+
+    std::cin >> m;
+    int* data2 = new int[m];
+
+    for (int i = 0; i < m; i++) {
+        std::cin >> data2[i];
+    }
+
+    std::cin >> find;
+    int k = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j + k - 1 < m; j++) {
+            if (data1[i] + data2[m - k - j] == find) {
+                count += 1;
+                k += j;
+                break;
+            }
+        }
+    }
+    std::cout << count;
     return 0;
 }
