aa8dc3f020d089cad17fcc08da4f8376cc2d11c8
David Beylin <57668741+davabey@users.noreply.github.com>
2021-10-21 11:59:06 +0500

    GG

--- main.cpp
+++ main.cpp
@@ -1,6 +1,98 @@
 #include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+
+int from_infix_to_num(std::string &infix) {
+    std::stack<std::string> stack;
+    std::queue<std::string> queue;
+    std::string postfix;
+
+    for (int i = 0; i < infix.length(); i++) {
+        if (infix[i] == '(') {
+            stack.push(infix.substr(i, 1));
+        } else if (infix[i] == '+' || infix[i] == '-' || infix[i] == '*' || infix[i] == '/') {
+            if (stack.empty() || stack.top() == "(") {
+                stack.push(infix.substr(i, 1));
+            } else if ((infix[i] == '*' || infix[i] == '/') && (stack.top() == "+" || stack.top() == "-")) {
+                stack.push(infix.substr(i, 1));
+            } else {
+                queue.push(stack.top());
+                stack.pop();
+                stack.push(infix.substr(i, 1));
+            }
+        } else if (infix[i] == ')') {
+            while (true) {
+                if (stack.top() == "(") {
+                    stack.pop();
+                    break;
+                }
+                queue.push(stack.top());
+                stack.pop();
+            }
+        } else {
+            int j = 0;
+            while ((infix[i + j] <= '9') && (infix[i + j] >= '0')) {
+                j++;
+            }
+            queue.push(infix.substr(i, j));
+            i += j - 1;
+        }
+    }
+
+    while (!stack.empty()) {
+        queue.push(stack.top());
+        stack.pop();
+    }
+
+    // из постфиксной в число
+    int operand1, operand2;
+    std::stack<int> stack_;
+
+    while (!queue.empty()) {
+        if ((queue.front()[0] >= '0') && (queue.front()[0] <= '9')) {
+            stack_.push(std::stoi(queue.front()));
+            queue.pop();
+        } else if (queue.front() == "+") {
+            operand1 = stack_.top();
+            stack_.pop();
+            operand2 = stack_.top();
+            stack_.pop();
+            stack_.push(operand1 + operand2);
+            queue.pop();
+        } else if (queue.front() == "-") {
+            operand1 = stack_.top();
+            stack_.pop();
+            operand2 = stack_.top();
+            stack_.pop();
+            stack_.push(operand2 - operand1);
+            queue.pop();
+        } else if (queue.front() == "*") {
+            operand1 = stack_.top();
+            stack_.pop();
+            operand2 = stack_.top();
+            stack_.pop();
+            stack_.push(operand2 * operand1);
+            queue.pop();
+        } else if (queue.front() == "/") {
+            operand1 = stack_.top();
+            stack_.pop();
+            operand2 = stack_.top();
+            stack_.pop();
+            stack_.push(operand2 / operand1);
+            queue.pop();
+        }
+    }
+    return stack_.top();
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string infix, postfix;
+    std::cin >> infix;
+    //infix = "200-(123+34*2)+(48-2)";
+
+    std::cout << from_infix_to_num(infix);
+    //num = from_postfix_to_num(postfix);
+
     return 0;
 }
