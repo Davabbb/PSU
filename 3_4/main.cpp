7b6089fdce672bf106b3a0a18f60ba69c17b0977
David Beylin <57668741+davabey@users.noreply.github.com>
2021-10-26 11:40:57 +0500

    бинарный поиск

--- 3_4/main.cpp
+++ 3_4/main.cpp
@@ -2,46 +2,62 @@
 
 #include <iostream>
 #include <cmath>
-#include <algorithm>
-#include <map>
+
+int BinarySearch (int* data, int find, int last, int first) {
+    while (first < last) {
+        int mid = (first + last) / 2;
+        if (data[mid] == find) {
+            return mid;
+        } if (first == last - 1) {
+            if (abs(find - data[first]) <= abs(data[last] - find)) {
+                return mid;
+            } else {
+                return mid + 1;
+            }
+        } if (find > data[mid]) {
+            first = mid;
+        } else {
+            last = mid;
+        }
+    }
+    return first;
+}
+
 
 int main() {
-    int n, m;
-
+    int n;
     std::cin >> n;
     int* data1 = new int[n];
-
     for (int i = 0; i < n; i++) {
         std::cin >> data1[i];
     }
 
+    int m;
     std::cin >> m;
     int* data2 = new int[m];
-    int* answer = new int[m];
-
-    std::map <int, int> ans;
-
     for (int i = 0; i < m; i++) {
         std::cin >> data2[i];
-        answer[i] = data2[i];
-    }
-    std::sort(data2, data2 + m);
-
-    int k = 0;
-    for (int i = 0; i < m; i++) {
-        int j = 0;
-        while (abs(data1[j + k] - data2[i]) > abs(data1[j + k + 1] - data2[i])) {
-            j++;
-        }
-        ans[data2[i]] = j + k;
-        if (j != 0) {
-            k += j - 1;
-        }
-    }
-    for (int i = 0; i < m; i++) {
-        std::cout << ans[answer[i]] << " ";
     }
 
+    for (int j = 0; j < m; j++) {
+        int sm, count = 1, i = 1;
+        while (i < n) {
+            if (data1[i - 1] > data2[j]) {
+                count = i - 1;
+                break;
+            } if (i > n) {
+                count = n - 1;
+                break;
+            }
+            i *= 2;
+        }
+        if (i > n - 1) {
+            count = n - 1;
+        }
+        sm = count / 2;
+        std::cout << BinarySearch(data1, data2[j], count, sm) << " ";
+    }
+    delete [] data1;
+    delete [] data2;
     return 0;
 }
-
